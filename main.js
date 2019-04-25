a21890e5a5e9d1eaacc45729309ca9657c99eafb
Deron <Deron@relevantdevelopment.tech>
2019-04-24 20:08:54 -0400

    Add all database functions

---
+++ main.js
@@ -0,0 +1,63 @@
+username = ""; 
+
+function addGame(game){ 
+    response=$.ajax({
+        type: "GET",
+        data: ({game: game}),
+        url: "https://www.relevantdevelopment.tech/GFTDatabaseConnector/addGame.php",
+        async: false
+    }).responseText; 
+    
+    return response; 
+}
+
+function logScore(game, user, score){
+    //the first time a score is recorded for a game, the game is added to the database. Therefore, we call addGame() every time
+    //we log a score in case the game doesn't exist. The server side knows this and only trys to add the game if it doesn't 
+    //exist in the database. 
+    addGame(game); 
+
+    //add the score for the user. This function will check whether or not the user exists and return 1 if the user doesn't exist. 
+    response=$.ajax({
+        type: "GET",
+        data: ({game: game, user: user, score: score}),
+        url: "https://www.relevantdevelopment.tech/GFTDatabaseConnector/logScore.php",
+        async: false
+    }).responseText; 
+    
+    return response; 
+}
+
+function getAllScores(){
+    response=$.ajax({
+        type: "GET",
+        url: "https://www.relevantdevelopment.tech/GFTDatabaseConnector/getAllScores.php",
+        async: false
+    }).responseText; 
+    
+    alert(response); 
+}
+
+function addUser(fName, lName, dob, password, username){
+    response=$.ajax({
+        type: "GET",
+        data: ({fName: fName, lName: lName, dob: dob, password: password, username: username}),
+        url: "https://www.relevantdevelopment.tech/GFTDatabaseConnector/addUser.php",
+        async: false
+    }).responseText; 
+    
+    if(response != "")
+        alert(response); 
+    else   
+        alert("Account added. Have fun!"); 
+}
+
+function checkUserLogin(username, password){
+    response=$.ajax({
+        type: "GET",
+        data: ({username: username, password: password}),
+        url: "https://www.relevantdevelopment.tech/GFTDatabaseConnector/loginUser.php",
+        async: false
+    }).responseText; 
+    alert(response); 
+}
